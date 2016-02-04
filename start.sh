13ccedddcb6035e75a97ec7ecc60118458998bd5
dsoares <diana.soares@gmail.com>
2016-02-04 12:11:00 +0000

    mkdir the dmat folder in DocumentRoot.

--- start.sh
+++ start.sh
@@ -49,6 +49,9 @@ if [[ "$TEMPLATE_NGINX_HTML" != "0" ]] ; then
   done
 fi
 
+# mkdir dmat site dir
+mkdir -p /usr/share/nginx/html/mat
+
 # Again set the right permissions (needed when mounting from a volume)
 chown -Rf www-data.www-data /usr/share/nginx/html/
 
